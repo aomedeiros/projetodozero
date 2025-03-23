c4c3de5a660704b4b935ecd64145fccff7a64040
aomedeiros <ao.medeiros@gmail.com>
2025-03-23 15:15:30 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("jornada de dados")
+
+# Variáveis
+idade = 53
+nome = "Adriano"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+lista = [1,2,3,4,5]
+
+# Metodos
+lista_invertida = lista.reverse()
